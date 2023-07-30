0e8342c6f670473888b59f0095ed02f86d974c5d
Melvin Guirchoun <guirchounmelvin@gmail.com>
2023-07-30 15:41:51 +0200

    changement d'emplacelement des images dans assets

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center hero" >
-    <img class="mb-4 app-logo" src="img/Capture.PNG" alt="" width="120" />
+    <img class="mb-4 app-logo" src="src/assets/img/Capture.PNG" alt="" width="120" />
     <h1 class="mb-4">Welcome to future direction</h1>
     <p class="lead">
       you need help finding your way, we offer orientation assistance for your future
