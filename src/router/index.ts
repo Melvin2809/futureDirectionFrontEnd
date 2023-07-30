dd637802b56e04cfc5199cc2765151b2972066e0
Melvin Guirchoun <guirchounmelvin@gmail.com>
2023-07-30 15:40:29 +0200

    importer les liens formations ecoles salaires

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter as createVueRouter, createWebHashHistory, Router } from "vue-router";
 import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
+import Formations from"../views/Formations.vue";
+import Ecoles from"../views/Ecoles.vue";
+import Salaires from"../views/Salaires.vue";
+
 import { createAuthGuard } from "@auth0/auth0-vue";
 import { App } from 'vue';
 
@@ -17,6 +21,21 @@ export function createRouter(app: App): Router {
         name: "profile",
         component: Profile,
         beforeEnter: createAuthGuard(app)
+      },
+      {
+        path: "/Formations",
+        name: "formations",
+        component: Formations
+      },
+      {
+        path: "/Ecoles",
+        name: "Ecoles",
+        component: Ecoles
+      },
+      {
+        path: "/Salaires",
+        name: "Salires",
+        component:Salaires
       }
     ],
     history: createWebHashHistory()
