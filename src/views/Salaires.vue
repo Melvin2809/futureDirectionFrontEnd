273df9e3111a98f5f6d03b8f41dc54a1981f2b9d
Melvin Guirchoun <guirchounmelvin@gmail.com>
2023-08-07 17:11:48 +0200

    affichage des universités dans salaires

--- src/views/Salaires.vue
+++ src/views/Salaires.vue
@@ -1,20 +1,80 @@
 <template>
-    <div>
-      <h1>Comparaisons de Salaires <br></h1>
-    </div>
-    <br><br>
-
-    
+  <div v-for="(universite, index) in universites" :key="index">
+    <a :ref="universite.salaire" target="_blank">
+      <div class="image-container">
+        <img :src="universite.image" alt="Clickable Image" @click="handleClick(universite)"><br>
+        <div class="hover-message">Cliquer pour afficher les Salaires</div>
+      </div>
+    </a>
+  </div>
+  <br><br>
 </template>
   
-  <script lang="ts">
- 
- import Hero from "../components/Hero.vue"; 
+<script lang="ts">
 
-  export default {
-    name: "Salaires-view",
-    components:{
-        Hero
+import Hero from "../components/Hero.vue";
+import jsonData from "@/ecolesUniversites.json"
+
+interface Universite {
+  nom: string;
+  adresse: string;
+  telephone: string;
+  image: string;
+  description: string;
+  reputation: string;
+  salaire: string;
+}
+
+
+export default {
+  name: "Salaires-view",
+  components: {
+    Hero
+  },
+  data() {
+    return {
+      universites: jsonData,
+    }
+  },
+  methods: {
+    handleClick(universite: Universite) {
+      window.open(universite.salaire, "_blank");
     }
   }
-  </script>
+}
+</script>
+  
+<style>
+.image-container {
+  position: relative;
+}
+
+.image-message-container {
+  display: flex;
+  align-items: center;
+} 
+
+.hover-message {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  display: none;
+}
+
+.image-container:hover {
+  display: block;
+  margin-left: 1px;
+  /*background-color: rgb(255, 255, 255);*/
+  box-shadow:0 0 10px #a7b4b1, 0 0 20px #c6d8d5, 0 0 30px #cadbd8;;
+  
+}
+
+
+
+
+</style>
