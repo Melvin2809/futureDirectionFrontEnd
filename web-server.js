0b5764b042e2e411c936a07ab22f266af9db99e6
Melvin Guirchoun <guirchounmelvin@gmail.com>
2023-08-09 10:57:37 +0200

    modifs

--- web-server.js
+++ web-server.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-console */
 const express = require("express");
+const cors = require('cors');
 const { join } = require("path");
 const morgan = require("morgan");
 const app = express();
+
+// Configure CORS options
+const corsOptions = {
+  origin: 'http://localhost:3000', // Mettez ici l'origine de votre application
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "dist")));
